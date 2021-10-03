2fd021dacd079437fa5b379a5bc72ba96e1c6cae
digitalrama <rama@digiralrama.co.in>
2021-10-03 15:33:30 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Rama <a href="https://nextjs.org">Next.js!</a>
+          Welome To <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
